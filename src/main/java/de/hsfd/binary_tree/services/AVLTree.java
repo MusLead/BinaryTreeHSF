5902d0a2269e5c8d5a1f598db3244f214923c52b
Agha Muhammad Aslam <aghaaslam246@gmail.com>
2025-01-18 23:22:00 +0100

    refactor: optimize AVL balance operation

--- src/main/java/de/hsfd/binary_tree/services/AVLTree.java
+++ src/main/java/de/hsfd/binary_tree/services/AVLTree.java
@@ -2,12 +2,8 @@ package de.hsfd.binary_tree.services;
 
 import de.hsfd.binary_tree.services.exceptions.TreeException;
 
-import static de.hsfd.binary_tree.services.AVLTree.OPERATION.DELETE;
-import static de.hsfd.binary_tree.services.AVLTree.OPERATION.INSERT;
-
 @SuppressWarnings({"rawtypes","unchecked"})
 public class AVLTree extends BinaryTree {
-    enum OPERATION {DELETE, INSERT}
 
     @Override
     protected Node deleteTarget(Node parentTarget, Node target, CHILD positionOfTarget) throws TreeException {
@@ -24,7 +20,7 @@ public class AVLTree extends BinaryTree {
                 possibleNullNode =  deleteTargetWithOneChildOrNone(parentTarget, target, positionOfTarget);
             }
             if(target.getParent() == null && target != root) target = parentTarget;
-            balanceTheTree(target,target,DELETE);
+            balanceTheTree(target);
             return possibleNullNode;
         }
         return null;
@@ -34,7 +30,7 @@ public class AVLTree extends BinaryTree {
     public void insert(Comparable x) throws TreeException {
         Node newNode = new Node(x);
         insertNode(newNode);
-        balanceTheTree(newNode.getParent(),newNode,INSERT);
+        balanceTheTree(newNode.getParent());
     }
 
     /**
@@ -61,32 +57,28 @@ public class AVLTree extends BinaryTree {
      * 
      *
      * @param parent the parent node from which balancing starts
-     * @param newNode the newly inserted or affected node requiring balance checks (especially for insert)
-     *                otherwise for delete should only be newNode == parent
      * @throws TreeException if a violation of AVL tree properties persists after balancing
      */
-    private void balanceTheTree(Node parent, Node newNode, OPERATION operation) throws TreeException {
+    private void balanceTheTree(Node parent) throws TreeException {
         while(parent != null) {
             updateHeight(parent);
-            int balance = getBalanceFactor(parent);
+            int balance = calculateBalanceFactor(parent);
 
             if(balance > 1 || balance < -1) {
                 if (balance > 0) { //left heavy from the parent
-                    if(operation.equals(DELETE) && getBalanceFactor(parent.getLeft()) < 0 // delete case
-                            || operation.equals(INSERT) &&  newNode.getData().compareTo(parent.getLeft().getData()) > 0) { //insert case
+                    if(calculateBalanceFactor(parent.getLeft()) < 0){
                         leftRotate(parent.getLeft());// Left Right Case
                     }
                     rightRotate(parent);
                 } else { // (balance < 0) right heavy from the parent
-                    if (operation.equals(DELETE) && getBalanceFactor(parent.getRight()) > 0 // delete case
-                            || operation.equals(INSERT) && newNode.getData().compareTo(parent.getRight().getData()) < 0){ //insert case
+                    if (calculateBalanceFactor(parent.getRight()) > 0 ){
                         rightRotate(parent.getRight());// Right Left Case
                     }
                     leftRotate(parent);
                 }
                 if(parent.getParent() == null) this.root = parent;
 
-                if (!(getBalanceFactor(parent) <= 1 && getBalanceFactor(parent) >= -1)) // this node to the leaf should now be balanced
+                if (!(calculateBalanceFactor(parent) <= 1 && calculateBalanceFactor(parent) >= -1)) // this node to the leaf should now be balanced
                     throw new TreeException("Violates the AVL tree rule\nNode: " +
                             parent.getData() +"\nHeight: " + parent.getHeight() + "\nTree:\n"
                             + this.getTreePrinter().prettyPrint());
@@ -104,7 +96,7 @@ public class AVLTree extends BinaryTree {
      * @param n the root to be checked whether imbalance exists
      * @return the integer result of the balance factor
      */
-    static int getBalanceFactor(Node n) {
+    static int calculateBalanceFactor(Node n) {
         if (n == null) // it means the node has not been initialised
             return 0;
         return height(n.getLeft()) - height(n.getRight());
@@ -129,7 +121,7 @@ public class AVLTree extends BinaryTree {
 
             // this code can only be executed if initialised height for a node is 0!
             if(parent.getHeight() > 1) {
-                int balance = getBalanceFactor(parent);
+                int balance = calculateBalanceFactor(parent);
                 if (balance > 0) { //left heavy from the parent
                     if (newNode.getData().compareTo(parent.getLeft().getData()) > 0) {
                         leftRotate(parent.getLeft()); // Left Right Case
@@ -151,57 +143,4 @@ public class AVLTree extends BinaryTree {
             parent = parent.getParent();
         }
     }
-
-    /**
-     * Deletes a target node from the AVL tree following AVL deletion rules.
-     * This method is deprecated and is preserved for historical purposes only.
-     * The deletion process ensures that the AVL balancing property is preserved.
-     *
-     * @deprecated this code does not work as it suppose to be. it has too complicated assumption
-     * that could make everything more complicated than it should be
-     * @param parentTarget the parent node of the target being deleted, or null if the target is the root
-     * @param target the node to be deleted
-     * @param positionOfTarget the position of the target node relative to its parent (left or right child)
-     * @throws TreeException if the deletion leads to a violation of the AVL tree properties
-     */
-    @Deprecated
-    @SuppressWarnings("unused")
-    protected void deleteTargetOld(Node parentTarget, Node target, CHILD positionOfTarget) throws TreeException {
-        // target found
-        if(parentTarget == null && target.isLeaf()) { // target is the root and only one
-            root = null;
-        } else {
-            Node targetRightChild = target.getRight();
-            if(target.getRight() != null && target.getLeft() != null) {
-                // Case 2 if the target has two children. This case is from the Lecture AlgoDS 24/25 HS Fulda
-                // Then take the inorder approach to find the smallest children of the right target's children
-                parentTarget = replaceWithTheSmallestOfRightChildren(target);
-            } else {
-                deleteTargetWithOneChildOrNone(parentTarget, target, positionOfTarget);
-            }
-
-            while(parentTarget != null){
-                Node node = parentTarget.getRight() != null && parentTarget.getRight() == targetRightChild ? parentTarget.getRight()
-//                        : parentTarget.getLeft() != null && parentTarget.getLeft() == targetRightChild ? parentTarget.getLeft()
-                        : parentTarget.getLeft();
-                if (node == null && parentTarget != root) { // if node is null, find predecessor to balance the tree
-                    node = parentTarget;
-                    parentTarget = parentTarget.getParent();
-                } else if (node != null) {
-                    // we assume if there is only one child/subtree in the node, that means
-                    // it might not be balanced further up. therefore take child of the node as node
-                    if(node.getLeft() == null && node.getRight() != null) node = node.getRight();
-                    if(node.getRight() == null && node.getLeft() != null) node = node.getLeft();
-                } else {
-                    // we assume that there is just one element in the tree
-                    node = parentTarget;
-                }
-                balanceTheTree(parentTarget,node,DELETE);
-                parentTarget = parentTarget == null ? null : parentTarget.getParent();
-            }
-
-        }
-
-    }
-
 }
